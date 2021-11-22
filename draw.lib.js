943479527707b5ac0f8daa789c0065bbffb1e22f
play.js <git@gituser.com>
2021-11-22 13:59:50 +0100

    fix #25

--- draw.lib.js
+++ draw.lib.js
@@ -34,10 +34,10 @@ class DrawLib {
     }
 
     blitc(spr, x, y, col) {
-      this.ctx.drawImage(this.sprites[spr], x - this.cx, y - this.cy);
-      this.ctx.globalCompositeOperation = "source-in";
       this.ctx.fillStyle = col;
       this.ctx.fillRect(0, 0, this.vw, this.vh);
+      this.ctx.globalCompositeOperation = "destination-in";
+      this.ctx.drawImage(this.sprites[spr], x - this.cx, y - this.cy);
       this.ctx.globalCompositeOperation = "source-over";
     }
 
